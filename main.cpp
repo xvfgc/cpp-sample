359b9b50ae7ab5fd3bad3349b4d4076d095b7bd2
x.x <38306136+xvfgc@users.noreply.github.com>
2024-09-20 10:44:55 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 
 void cpp_print(int v) {
-    if(v > 0){
+    if(v < 1){
         std::cout << "C++: Hello, World! - 0\n";
     }else{
         std::cout << "C++: Hello, World! - 1\n";
